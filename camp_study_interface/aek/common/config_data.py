f676f20ac561de1667942138ca2ae1d6696561a9
王敏 <13758206408@163.com>
2023-02-15 22:19:52 +0800

    test1

--- camp_study_interface/aek/common/config_data.py
+++ camp_study_interface/aek/common/config_data.py
@@ -9,20 +9,10 @@ warehouseIdOne = ''
 
 #2级库
 warehouseIdTwo = ''
-# x-auth-token = 'eyxxcFoxODdrbk1DWG85QTdxY3pwakNoUGVvVEM1Ym1BPSJ9'
-# #中昌token 下游
-# DOWNSTREAM_TOKEN='eyxPSJ9'
-# DOWNSTREAM_HEADERS={
-#     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-#     'token':DOWNSTREAM_TOKEN,
-#     'Content-Type':'application/json'
-# }
-# #三笑token 上游
-# UPSTREAM_TOKEN='eyJlbmNyeXB0HFUN3d1Q5QfQ=='
-# UPSTREAM_HEADERS={
-#     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-#     'token':UPSTREAM_TOKEN,
-#     'Content-Type':'application/json'
-# }
-#数据库配置，使用数据库操作时使用
-# MYSQL_CONFIG=("101.xx.xx.xx",3306, 'root', '123456', 'drf')
+
+#数据库
+MYSQL_CONFIG=('localhost',3306, 'root', 'root', 'demo')
+
+
+
+
